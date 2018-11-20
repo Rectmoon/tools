f3e166f65a9e62c0be377ad7f1af64e759801fd5
Rectmoon <1426890675@qq.com>
2018-11-21 01:56:59 +0800

    'ok'

--- qinchuan/.eslintrc.js
+++ qinchuan/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: 'eslint:recommended',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
--- qinchuan/src/main.js
+++ qinchuan/src/main.js
@@ -6,10 +6,14 @@ sleep(2).then(() => {
   console.log(343)
 })
 
+async function fn() {
+  await sleep(4)
+  console.log(567)
+}
 function f() {
   console.log(123)
 }
-
+fn()
 export default {
   add: (pre, next) => pre + next
 }
