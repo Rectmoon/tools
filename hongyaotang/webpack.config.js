dcdda66c842705909dd48d2d452f4112aeb720e6
Rectmoon <1426890675@qq.com>
2018-11-26 00:59:46 +0800

    ok

--- hongyaotang/webpack.config.js
+++ hongyaotang/webpack.config.js
@@ -2,8 +2,6 @@ const mode = process.env.NODE_ENV.trim()
 const analyze = process.env.npm_config_report
 const zip = process.env.npm_config_zip
 
-console.log(analyze)
-console.log(zip)
 const compiler = require(`./build`)
 
 module.exports = compiler(mode, { option: { analyze, zip } })
