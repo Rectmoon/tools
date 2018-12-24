0349fc1bbd25154067e2d77b1f20fb3db9639f8f
Rectmoon <1426890675@qq.com>
2018-12-24 20:41:23 +0800

    '1'

--- hongyaotang/.postcssrc.js
+++ hongyaotang/.postcssrc.js
@@ -15,7 +15,7 @@ module.exports = {
       rootValue: 50,
       unitPrecision: 5,
       propWhiteList: [],
-      selectorBlackList: [/header$/, /footer$/, '.vux-', '.veui-', '.mt-', '.mint-']
+      selectorBlackList: [/header$/, /footer$/, '.vux-', '.weui-', '.mt-', '.mint-']
     },
     autoprefixer: {
       browsers: ['last 5 version', 'Android >= 4.0'],
