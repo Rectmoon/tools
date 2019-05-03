e41eff21384515edea3e6dec2e6d8803ffb593f8
Rectmoon <15692087320@163.com>
2019-05-04 01:00:51 +0800

    删除不必要的东西

--- rcie8/patches/document-head.js
+++
@@ -1,4 +0,0 @@
-// IE8 didn't have `document.head`
-if (!document.head) {
-  document.head = document.getElementsByTagName('head')[0];
-}
--- rcie8/src/components/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import style from '../styles/app.css';
-
-
-export default () => (
-	<div className={style['NotFound']}>
-		The page was not found !
-	</div>
-);
