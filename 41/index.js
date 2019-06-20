893e715567c82e019b8ba84e9c7eead4b09bef3e
Rectmoon <15692087320@163.com>
2019-06-20 23:43:37 +0800

    1

--- 41/index.js
+++ 41/index.js
@@ -1,7 +1,16 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(express.static('./dist'))
-app.use(express.static('./dist/pages', { extensions: ['html'] }))
+app.use(express.static(path.join(__dirname, 'dist'), {
+   maxage: 0
+}))
+// app.use(express.static('./dist'))
+// app.use(express.static('./dist/pages', { extensions: ['html'] }))
+
+app.use("*", (req, res, next) => {
+	res.end('404')
+})
 
 app.listen(3000)
+
