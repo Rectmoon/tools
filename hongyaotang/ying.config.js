63ec7aa3a67f9977688a655e6199fc6f3d70c7e9
Rectmoon <15692087320@163.com>
2019-08-19 00:43:31 +0800

    1

--- hongyaotang/ying.config.js
+++ hongyaotang/ying.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  useExternals: false, // 如果多页面所用技术栈不一致, 个人觉得使用externals比较合适, 否则多页面都需要引入dll.js会比较麻烦
-  useDll: true,
+  useExternals: true, // 如果多页面所用技术栈不一致, 个人觉得使用externals比较合适, 否则多页面都需要引入dll.js会比较麻烦
+  useDll: false,
   extractEntries: {
     vendor: ['jquery', 'axios'],
     vue: ['vue'],
