0c239db290689740bf22aa1cbe9cd6d5f40765d4
Rectmoon <15692087320@163.com>
2019-08-21 22:20:47 +0800

    ok

--- hongyaotang/app.js
+++ hongyaotang/app.js
@@ -2,16 +2,25 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use(
-  express.static(path.join(__dirname, 'dist'), {
-    maxage: 0
-  })
-)
+const staticPath = process.env.static || 'dist'
+const port = process.env.port || 3000
 
-app.use('*', (req, res, next) => {
-  res.end('404')
+app.use(express.static(path.join(__dirname, staticPath), { maxAge: '3d' }))
+
+app.use('/**', function(req, res) {
+  res.sendfile(staticPath + '/index.html', { maxAge: 0 })
 })
 
-app.listen(3000, () => {
-  console.log('app is listening at port 3000')
+app.listen(port, () => {
+  console.log(`app is listening at port ${port}`)
 })
+
+/**
+ * from memory cache:
+ * 资源直接从内存中获取，当关闭该页面时，此资源就会被内存释放掉，
+ * 再次重新打开相同页面时不会出现from memory cache的情况。
+ *
+ * from disk cache
+ * 资源从磁盘当中取出的，不会请求服务器但是此资源不会随着该页面的关闭而释放掉，
+ * 因为是存在硬盘当中的，下次打开仍会from disk cache
+ */
